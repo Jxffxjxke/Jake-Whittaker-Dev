1e9356f4e0b0307544aec54721404e8959fe2944
Jake.Whittaker <jaffajake25@gmail.com>
2025-03-24 08:42:54 +0000

    Adds nav pages

---
+++ src/app/components/JakeWhittaker.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const JakeWhittaker = () => {
+  return <div>JakeWhittaker</div>;
+};
--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -4,6 +4,12 @@ import { Tabs, Tab, Box } from "@mui/material";
 import { useState } from "react";
 import "../styles/NavBar.css";
 import CustomTabPanel from "./CustomTabPanel";
+import { JakeWhittaker } from "./JakeWhittaker";
+import { About } from "./About";
+import { Skills } from "./Skills";
+import { Projects } from "./Projects";
+import { Contact } from "./Contact";
+import { Resume } from "./Resume";
 
 export default function NavBar() {
   const [tabValue, setTabValue] = useState(1);
@@ -18,22 +24,31 @@ export default function NavBar() {
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <Tabs value={tabValue} onChange={handleTabChange}>
             <Tab label="Jake Whittaker" value={0} />
-              <Tab label="About" value={1} />
-              <Tab label="Skills" value={2} />
-              <Tab label="Projects" value={3} />
-              <Tab label="Contact" value={4} />
-              <Tab label="Resume" value={5} />
+            <Tab label="About" value={1} />
+            <Tab label="Skills" value={2} />
+            <Tab label="Projects" value={3} />
+            <Tab label="Contact" value={4} />
+            <Tab label="Resume" value={5} />
           </Tabs>
         </Box>
       </div>
       <CustomTabPanel value={tabValue} index={0}>
-        Item One
+        <JakeWhittaker />
       </CustomTabPanel>
       <CustomTabPanel value={tabValue} index={1}>
-        Item Two
+        <About />
       </CustomTabPanel>
       <CustomTabPanel value={tabValue} index={2}>
-        Item Three
+        <Skills />
+      </CustomTabPanel>
+      <CustomTabPanel value={tabValue} index={3}>
+        <Projects />
+      </CustomTabPanel>
+      <CustomTabPanel value={tabValue} index={4}>
+        <Contact />
+      </CustomTabPanel>
+      <CustomTabPanel value={tabValue} index={5}>
+        <Resume />
       </CustomTabPanel>
     </main>
   );
